1dd02457629768ab890380f8a5cd58cfc951e9c3
Sivabalarasu123 <83056155+Sivabalarasu123@users.noreply.github.com>
2021-04-25 21:36:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+            {
+                p = document.getElementById("principal").value;
+                n = document.getElementById("years").value;
+                r = document.getElementById("rate").value;
+                result = document.getElementById("result");
+                
+                result.innerHTML =(p*n*r/100);
+                
+                
+            }
